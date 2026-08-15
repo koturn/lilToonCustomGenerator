aa906ebc67a3c57705892b4fbbd21b065c14628f
koturn <jeak.koutan.apple@gmail.com>
2026-08-15 14:12:24 +0900

    chore: Bump up version number: 0.2.0 -> 1.0.0

--- Assets/koturn/Tools/lilToonCustomGenerator/AssemblyInfo.cs
+++ Assets/koturn/Tools/lilToonCustomGenerator/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 // Metadata
 [assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/lilToonCustomGenerator")]
